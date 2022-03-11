c89fb8c6e3aa60c0268687c2bf2cb339129f3ee7
Simon Wiles <simonjwiles@gmail.com>
2022-03-11 10:49:56 -0800

    Add a yarn/npm script to fetch and assemble data and dump to stdout
    Useful for development, probably for other stuff.  Note: required node's `--experimental-fetch` flag, available only in `<= v17.5.0`.

--- src/dataset.mjs
+++ src/dataset.mjs
@@ -8,4 +8,13 @@ const records = await neatCsv(await response.text(), {
   mapValues: ({ header, index, value }) => (value === "--" ? null : value),
 });
 
+import path from "path";
+import { fileURLToPath } from "url";
+
 export { records };
+
+// If called as a node script, print records to stdout.
+// See `yarn print-dataset`  (requires node >= v17.5.0)
+const nodePath = path.resolve(process.argv[1]);
+const modulePath = path.resolve(fileURLToPath(import.meta.url));
+if (nodePath === modulePath) console.log(records);
