86293c4c3c7224b1a7d98d24835725377ee87d3e
Simon Wiles <simonjwiles@gmail.com>
2022-05-19 12:30:09 -0700

    Popup glosses using `floating-ui`
    This will likely need to be more sophisticated; using any block-level markup in the glosses borks the markdown markup, so having the popup elements out-of-tree might make more sense (can deduplicate (and possibly pre-render?) that way too).
    
    This will do as the basis for further thought and discussion, at least.

--- src/glossary.mjs
+++ src/glossary.mjs
@@ -1,15 +1,56 @@
+import { computePosition, flip, shift, offset } from "@floating-ui/dom";
+
 const documents = document.querySelectorAll(".document .translation");
 
 const glossary = {
   heemraden: "Definition of heemraden",
 };
 
+const showGloss = (event) => {
+  const termSpan = event.target;
+  const glossSpan = termSpan.nextElementSibling;
+  glossSpan.style.display = "block";
+  update(termSpan, glossSpan);
+};
+
+const hideGloss = (event) => {
+  const termSpan = event.target;
+  const glossSpan = termSpan.nextElementSibling;
+  glossSpan.style.display = "";
+};
+
+const eventMap = [
+  ["mouseenter", showGloss],
+  ["mouseleave", hideGloss],
+  ["focus", showGloss],
+  ["blur", hideGloss],
+];
+
+const update = (termSpan, glossSpan) => {
+  computePosition(termSpan, glossSpan, {
+    placement: "top",
+    middleware: [offset(6), flip(), shift({ padding: 5 })],
+  }).then(({ x, y }) => {
+    Object.assign(glossSpan.style, {
+      left: `${x}px`,
+      top: `${y}px`,
+    });
+  });
+};
+
 const markupDoc = (doc) => {
   Object.keys(glossary).forEach((term) => {
     doc.innerHTML = doc.innerHTML.replace(
       new RegExp(term, "g"),
-      `<span class="glossary">${term}</span>`,
+      `<span class="glossary" aria-describedby="tooltip">${term}</span>
+       <span role="tooltip" class="gloss"><span class="header">${term}</span>${glossary[term]}</span>`,
     );
+  });
+
+  doc.querySelectorAll(".glossary").forEach((termSpan) => {
+    eventMap.forEach(([event, listener]) => {
+      termSpan.addEventListener(event, listener);
+    });
   });
 };
 
