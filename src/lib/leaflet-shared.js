1fc521a1c605d25a81336390236082a14be389f6
Simon Wiles <simonjwiles@gmail.com>
2022-05-21 17:08:39 -0700

    Use marker from `<Icon/>` rather than inlining in multiple place

--- src/lib/leaflet-shared.js
+++ src/lib/leaflet-shared.js
@@ -2,12 +2,7 @@ import * as L from "leaflet/dist/leaflet-src.esm.js";
 
 export const icon = L.divIcon({
   className: "marker",
-  html: `
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" class="marker">
-        <path class="shadow" d="M16 32s1.427-9.585 3.761-12.025c4.595-4.805 8.685-.99 8.685-.99s4.044 3.964-.526 8.743C25.514 30.245 16 32 16 32z"/>
-        <path class="pin" d="M15.938 32S6 17.938 6 11.938C6 .125 15.938 0 15.938 0S26 .125 26 11.875C26 18.062 15.938 32 15.938 32zM16 6a4 4 0 100 8 4 4 0 000-8z"/>
-      </svg>
-    `,
+  html: document.querySelector("svg.marker").outerHTML,
   iconSize: [40, 40],
   iconAnchor: [20, 40],
   popupAnchor: [0, -45],
