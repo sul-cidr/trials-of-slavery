189ba156b1628f60a8d5e7a381f513dbc95b742f
Simon Wiles <simonjwiles@gmail.com>
2022-05-23 14:59:24 -0700

    Add a static JSON endpoint converted from the glossary source

---
+++ src/pages/glossary.json.js
@@ -0,0 +1,26 @@
+import fs from "node:fs";
+
+const allFileContents = fs.readFileSync(
+  "src/documents/TrialsGlossary.md",
+  "utf-8",
+);
+
+const glossary = {};
+let currentKey = [];
+
+allFileContents.split(/\r?\n/).forEach((line) => {
+  if (line === "") return;
+
+  if (line.startsWith("-")) {
+    currentKey.push(line.replace(/^-\s+/, ""));
+  } else {
+    glossary[currentKey] = line;
+    currentKey = [];
+  }
+});
+
+export async function get() {
+  return {
+    body: JSON.stringify(glossary),
+  };
+}
