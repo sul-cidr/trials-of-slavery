99e8dc0f5b4a186a90e57e0d9864950ca989cb8e
Simon Wiles <simonjwiles@gmail.com>
2022-06-13 12:22:27 -0700

    Remove plugins from generic markdown parsing pipeline

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,6 @@
 // Full Astro Configuration API Documentation:
 // https://docs.astro.build/reference/configuration-reference
 
-import rehypeTrialsGlossary from "./lib/rehype-trials-glossary.mjs";
-
 // @ts-check
 export default /** @type {import('astro').AstroUserConfig} */ ({
   // Comment out "renderers: []" to enable Astro's default component support.
@@ -10,10 +8,6 @@ export default /** @type {import('astro').AstroUserConfig} */ ({
   base: "/trials-of-slavery/",
   trailingSlash: "always",
   integrations: [],
-  markdown: {
-    remarkPlugins: ["remark-gfm", "remark-supersub"],
-    rehypePlugins: [rehypeTrialsGlossary],
-  },
   vite: {
     ssr: { external: ["neat-csv"] },
     optimizeDeps: { exclude: ["neat-csv"] },
