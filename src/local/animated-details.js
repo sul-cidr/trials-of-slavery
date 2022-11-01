b8cf1cf83023b12be1d2dccce8a35ce0e067deb6
Simon Wiles <simonjwiles@gmail.com>
2022-11-01 11:49:57 -0700

    WAAPI `AnimatedDetails` class

---
+++ src/local/animated-details.js
@@ -0,0 +1,85 @@
+export class AnimatedDetails {
+  constructor(el, options = {}) {
+    const defaultOptions = {
+      contentSelector: ".content",
+      duration: 400,
+      easing: "ease-out",
+    };
+
+    this.options = { ...defaultOptions, ...options };
+
+    this.el = el;
+    this.summary = el.querySelector("summary");
+    this.content = el.querySelector(this.options.contentSelector);
+    this.animation = null;
+    this.isClosing = false;
+    this.isExpanding = false;
+    this.summary.addEventListener("click", (event) => this.onClick(event));
+  }
+
+  onClick(event) {
+    event.preventDefault();
+    this.el.style.overflow = "hidden";
+    if (this.isClosing || !this.el.open) {
+      this.open();
+    } else if (this.isExpanding || this.el.open) {
+      this.shrink();
+    }
+  }
+
+  shrink() {
+    this.isClosing = true;
+    const startHeight = `${this.el.offsetHeight}px`;
+    const endHeight = `${this.summary.offsetHeight}px`;
+    if (this.animation) this.animation.cancel();
+
+    this.animation = this.el.animate(
+      {
+        height: [startHeight, endHeight],
+      },
+      {
+        duration: this.options.duration,
+        easing: this.options.easing,
+      },
+    );
+
+    this.animation.onfinish = () => this.onAnimationFinish(false);
+    this.animation.oncancel = () => (this.isClosing = false);
+  }
+
+  open() {
+    this.el.style.height = `${this.el.offsetHeight}px`;
+    this.el.open = true;
+    window.requestAnimationFrame(() => this.expand());
+  }
+
+  expand() {
+    this.isExpanding = true;
+    const startHeight = `${this.el.offsetHeight}px`;
+    const endHeight = `${
+      this.summary.offsetHeight + this.content.offsetHeight
+    }px`;
+
+    if (this.animation) this.animation.cancel();
+
+    this.animation = this.el.animate(
+      {
+        height: [startHeight, endHeight],
+      },
+      {
+        duration: this.options.duration,
+        easing: this.options.easing,
+      },
+    );
+    this.animation.onfinish = () => this.onAnimationFinish(true);
+    this.animation.oncancel = () => (this.isExpanding = false);
+  }
+
+  onAnimationFinish(open) {
+    this.el.open = open;
+    this.animation = null;
+    this.isClosing = false;
+    this.isExpanding = false;
+    this.el.style.height = this.el.style.overflow = "";
+  }
+}
