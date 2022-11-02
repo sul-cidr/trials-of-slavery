fcc3257a128b1dc95355fb9d0e6a60f6b047374d
Simon Wiles <simonjwiles@gmail.com>
2022-11-02 09:48:19 -0700

    Add citation and (fixed) language to index

--- bin/build-index.mjs
+++ bin/build-index.mjs
@@ -2,7 +2,7 @@ import fs from "fs";
 import path from "path";
 import glob from "glob";
 
-import { documentMdToHtml } from "../lib/markdown-parsing.mjs";
+import { documentMdToHtml, getFrontmatter } from "../lib/markdown-parsing.mjs";
 import siteConfig from "../site-config.json" assert { type: "json" };
 
 const { documentsBasePath, idxDir } = siteConfig;
@@ -13,12 +13,18 @@ fs.mkdirSync(idxDir, { recursive: true });
 
 for (let filePath of filePaths) {
   const filename = path.parse(filePath).name;
+  const frontmatter = getFrontmatter(filePath);
   const title = /^\d+\s/.test(filename)
     ? path.basename(path.dirname(filePath)).slice(3)
     : filename;
   const html = await documentMdToHtml(filePath);
+
   fs.writeFileSync(
     `${idxDir}/${filename}.html`,
-    `<html><h1>${title}</h1>${html}</html`,
+    `<html lang="en">
+      <h1>${title}</h1>
+      <span data-pagefind-meta="citation:${frontmatter.citation}"></span>
+      ${html}
+    </html>`,
   );
 }
