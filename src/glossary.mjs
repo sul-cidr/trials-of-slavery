de831e396d5f84e1c09f1a8316ae702a462e47be
Simon Wiles <simonjwiles@gmail.com>
2022-11-10 17:18:15 -0800

    Attach event handlers to glossary terms in Dutch documents too
    Relevant for footnotes.

--- src/glossary.mjs
+++ src/glossary.mjs
@@ -1,7 +1,7 @@
 import { computePosition, flip, shift, offset } from "@floating-ui/dom";
 
 const documents = document.querySelectorAll(
-  ".document [data-doc-lang='translation'], .summary > div",
+  ".document [data-doc-lang], .summary > div",
 );
 
 const showGloss = (event) => {
