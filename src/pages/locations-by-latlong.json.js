33672319430eb842a644ba6b934ee8f600f76f2f
Simon Wiles <simonjwiles@gmail.com>
2025-11-05 11:49:18 -0800

    Don't plot locations with no lat/long on the main map

--- src/pages/locations-by-latlong.json.js
+++ src/pages/locations-by-latlong.json.js
@@ -3,20 +3,22 @@ import { records } from "@dataset";
 const locations = records.map((record) => record["trial_locations"]).flat();
 
 // create an object where keys are lat,long pairs and values are arrays of locations
-const locationsByLatlong = locations.reduce(
-  (result, location) => ({
-    ...result,
-    [`${location.Latitude},${location.Longitude}`]: [
-      ...(result[`${location.Latitude},${location.Longitude}`] || []),
-      (() => {
-        // iife to pick only needed properties
-        const { "Case ID": caseId, Location: name, ...rest } = location;
-        return { caseId, name };
-      })(),
-    ],
-  }),
-  {},
-);
+const locationsByLatlong = locations
+  .filter((location) => !(!location.Latitude || !location.Longitude))
+  .reduce(
+    (result, location) => ({
+      ...result,
+      [`${location.Latitude},${location.Longitude}`]: [
+        ...(result[`${location.Latitude},${location.Longitude}`] || []),
+        (() => {
+          // iife to pick only needed properties
+          const { "Case ID": caseId, Location: name, ...rest } = location;
+          return { caseId, name };
+        })(),
+      ],
+    }),
+    {},
+  );
 
 export async function get() {
   return {
